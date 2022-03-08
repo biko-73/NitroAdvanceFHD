51078054641cdb712e9ec63eee9c17988e4e4a7e
biko-73 <57875905+biko-73@users.noreply.github.com>
2022-03-09 01:32:54 +0200

    T

---
+++ installer.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#####################################################
+version=5.0
+description="Have Fun With NitroAdvancedHD Skin !!!"
+#####################################################
+
+TEMPATH='/tmp'
+PLUGINPATH='/usr/lib/enigma2/python/Plugins/Extensions/NitroAdvanceFHD'
+SKINPATH='/usr/share/enigma2/NitroAdvanceFHD'
+
+CHECK='/tmp/check'
+NITRO='/tmp/nitroadvancefhd/usr/*'
+NITROSKIN='/tmp/nitroadvancefhd/share/*'
+
+uname -m >$CHECK
+
+# remove old version
+rm -rf $PLUGINPATH >/dev/null 2>&1
+rm -rf $SKINPATH >/dev/null 2>&1
+
+cd $TEMPATH
+set -e
+wget -q "http://ipkinstall.ath.cx/nitro/nitroadvancefhd-$version.tar.gz"
+
+tar -xzf nitroadvancefhd-"$version".tar.gz -C /tmp
+set +e
+rm -f nitroadvancefhd-"$version".tar.gz
+cd ..
+
+if grep -qs -i 'mips' cat $CHECK; then
+        echo "[ Your device is MIPS ]"
+elif grep -qs -i 'armv7l' cat $CHECK; then
+        echo "[ Your device is armv7l ]"
+elif grep -qs -i 'sh4' cat $CHECK; then
+        echo "[ Your device is sh4 ]"
+else
+        echo "###############################"
+        echo "## Your stb is not supported ##"
+        echo "###############################"
+        rm -r /tmp/nitroadvancefhd
+        rm -f $CHECK
+        exit 1
+        echo ""
+fi
+echo "[ Installing New Skin Update Please Wait ... ]"
+#cp -a $NITRO $PLUGINPATH
+#cp -a $NITROSKIN $SKINPATH
+mkdir -p $PLUGINPATH
+cp -r $NITRO $PLUGINPATH
+mkdir -p $SKINPATH
+cp -r $NITROSKIN $SKINPATH
+sleep 2
+
+rm -r /tmp/nitroadvancefhd
+rm -f $CHECK
+
+echo ""
+sync
+echo "#########################################################"
+echo "#  NitroAdvanceFHD Skin $version INSTALLED SUCCESSFULLY #"
+echo "#                BY BIKO - support on                   #"
+echo "#   https://www.tunisia-sat.com/forums/forums/182/      #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+killall -9 enigma2
+exit 0
