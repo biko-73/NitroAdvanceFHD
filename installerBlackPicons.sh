5aa412618e723fbcb63d7ea2235e7e9ad614df59
biko-73 <57875905+biko-73@users.noreply.github.com>
2022-07-15 07:00:32 +0200

    s6

--- installerBlackPicons.sh
+++ installerBlackPicons.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/biko-73/NitroAdvanceFHD/main/installerTransparentPicons.sh -O - | /bin/sh
+##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/biko-73/NitroAdvanceFHD/main/installerBlackPicons.sh -O - | /bin/sh
 ## This script to downlaod and install Black Picon by () to inside plugin
 
-# Download and install Transparent Picons
+# Download and install Black Picons
 cd /tmp
 set -e
-wget "https://github.com/biko-73/NitroAdvanceFHD//raw/main/TransparentPicons.tar.gz"
-tar -xzf TransparentPicons.tar.gz -C /
+wget "https://github.com/biko-73/NitroAdvanceFHD//raw/main/BlackPicons.tar.gz"
+tar -xzf BlackPicons.tar.gz -C /
 set +e
 cd ..
 
 ### delete tmp files
-rm -f /tmp/TransparentPicons.tar.gz
+rm -f /tmp/BlackPicons.tar.gz
 sync
 echo ""
 echo ""
