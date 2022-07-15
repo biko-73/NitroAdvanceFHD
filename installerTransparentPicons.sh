c476c65d18c298c5c99c11df01b5d9723bab2881
biko-73 <57875905+biko-73@users.noreply.github.com>
2022-07-15 06:54:41 +0200

    S4

--- installerTransparentPicons.sh
+++ installerTransparentPicons.sh
@@ -2,6 +2,21 @@
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/biko-73/NitroAdvanceFHD/main/installerTransparentPicons.sh -O - | /bin/sh
 ## This script to downlaod and install Black Picon by () to inside plugin
 
+if [ ! -d /usr/lib64 ]; then
+	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
+else
+	PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/NitroAdvanceFHD
+fi
+
+## Remove old file from tmp directory
+[ -r /tmp/TransparentPicons.tar.gz ] && rm -f /tmp/TransparentPicons.tar.gz
+
+### Remove Currenty file from plugin
+rm -rf $PLUGINPATH/PICONS/emu
+rm -rf $PLUGINPATH/PICONS/piconSat
+rm -rf $PLUGINPATH/PICONS/piconProv
+rm -rf $PLUGINPATH/PICONS/piconCrypt
+
 # Download and install Transparent Picons
 cd /tmp
 set -e
@@ -16,6 +31,14 @@ sync
 echo ""
 echo ""
 echo "#########################################################"
-echo "#               picon is downloaded                     #"
+echo "#       NitroAdvanceFHD INSTALLED SUCCESSFULLY          #"
+echo "#                     By Biko                           #"              
+echo "#                     support                           #"
+echo "#  https://www.tunisia-sat.com/forums/threads/00000000  #"
+echo "#########################################################"
+echo ""
+echo ""
+echo "#########################################################"
+echo "#            Press ok to Exit from Console              #"
 echo "#########################################################"
 exit 0
